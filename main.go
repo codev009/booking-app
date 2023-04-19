faf7398163b0782deb44e5532bda9f5000388207
codev009 <lipin007@gmail.com>
2023-04-19 17:14:30 +0530

    add logic for remianing tickets

--- main.go
+++ main.go
@@ -3,12 +3,33 @@ package main
 import "fmt"
 
 func main() {
-  var conferenceName = "Go Conference"
-  const conferenceTickets = 50
-  var remainingTickets = 50
+  conferenceName := "Go Conference"
+  const conferenceTickets int  = 50
+  var remainingTickets uint = 50
 
-  fmt.Println("Welcome to", conferenceName, "booking application")
-  fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are remianing!")
+  fmt.Printf("Welcome to %v booking application\n", conferenceName)
+  fmt.Printf("We have total of %v tickets and %v are remianing!\n", conferenceTickets, remainingTickets)
   fmt.Println("Get your tickets here to attend")
 
+  var firstName string
+  var lastName string
+  var email string
+  var userTickets uint
+  // ask user for their name
+  fmt.Println("Enter your first name: ")
+  fmt.Scan(&firstName)
+
+  fmt.Println("Enter your last name: ")
+  fmt.Scan(&lastName)
+
+  fmt.Println("Enter your email address: ")
+  fmt.Scan(&email)
+  
+  fmt.Println("Enter number of tickets: ")
+  fmt.Scan(&userTickets)
+
+  remainingTickets = remainingTickets - userTickets
+  
+  fmt.Printf("Thank you %v %v for bokking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+  fmt.Printf("%v tickets remianing for %v\n", remainingTickets, conferenceName)
 }
